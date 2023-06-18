a482b2d17ef13b549aeea94c9887da80bc237b3a
paulohborba <paulohbr18@gmail.com>
2023-06-18 18:35:03 -0300

    Adicionando codigo

---
+++ app/src/js/app.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "../../node_modules/firebase/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA224zrzRZ7Iy7rrNRE9dWCUs-Kr23ktiE",
+  authDomain: "disciplinareq.firebaseapp.com",
+  databaseURL: "https://disciplinareq-default-rtdb.firebaseio.com",
+  projectId: "disciplinareq",
+  storageBucket: "disciplinareq.appspot.com",
+  messagingSenderId: "292130692921",
+  appId: "1:292130692921:web:29ab9b610e1c220d5652e8",
+  measurementId: "G-FLC6D2WGBC"
+};
+
+const firebase = initializeApp(firebaseConfig);

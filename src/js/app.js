9a0d4e4ec17587e6e1ddf6f1525de27bcdf4f2a4
lucianosgit <luciano.abs.work@gmail.com>
2023-06-22 09:46:10 -0300

    Isolamento e refatoração do back

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+const application = initializeApp({
+    apiKey: "AIzaSyA224zrzRZ7Iy7rrNRE9dWCUs-Kr23ktiE",
+    authDomain: "disciplinareq.firebaseapp.com",
+    databaseURL: "https://disciplinareq-default-rtdb.firebaseio.com",
+    projectId: "disciplinareq",
+    storageBucket: "disciplinareq.appspot.com",
+    messagingSenderId: "292130692921",
+    appId: "1:292130692921:web:29ab9b610e1c220d5652e8",
+    measurementId: "G-FLC6D2WGBC"
+});
+export { application };
